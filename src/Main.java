6ed815c85c16cf0c67d7b2d7d70d002a78816cdf
Rimanas <rimanas_alina@mail.ru>
2020-10-21 17:43:02 +0300

    Add Main.java and BankService.java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        BankService bankService = new BankService();
+        int result = bankService.calculateIncome(1000_00, 7);
+        System.out.println("Ваш доход составит:");
+        System.out.println(result);
+        int cashback = bankService.calculateCashback(1_000_000_00, 5);
+        System.out.println("Ваш кэшбэк составит:");
+        System.out.println(cashback);
+    }
+}
